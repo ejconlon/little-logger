09dfbf74fe0bd078a65ffae7c3f076e1ec562082
Eric Conlon <ejconlon@gmail.com>
2020-05-07 17:45:52 -0700

    init

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import qualified Littlelogger.Main
+import Prelude
+
+main :: IO ()
+main = Littlelogger.Main.main
---
+++ src/Littlelogger/Main.hs
@@ -0,0 +1,6 @@
+module Littlelogger.Main where
+
+import Prelude
+
+main :: IO ()
+main = putStrLn "hello, world"
